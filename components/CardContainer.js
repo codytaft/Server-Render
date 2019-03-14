d3a52ee7032db059ba6f8dfc0578e6db690441e6
codytaft <cody.taft@gmail.com>
2019-03-13 22:26:15 -0600

    Map and render cards in CardContainer

--- components/CardContainer.js
+++ components/CardContainer.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import events from '../static/mock_events.json';
+import Card from '../components/Card';
 
 export default function cardContainer() {
-  return events.map(event => {
-    return (
-      <div className='card-container'>
-        <p className='cards'>hi</p>
-      </div>
-    );
-  });
+  const cards = events.map(event => <Card />);
+  return (
+    <div className='card-container'>
+      {cards}
+      <style>
+        {`
+        .card-container {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-around;
+          flex-wrap: wrap
+        }
+      `}
+      </style>
+    </div>
+  );
 }
