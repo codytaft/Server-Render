11cedd902c037435b1187fb2b01113b5b87814b7
codytaft <cody.taft@gmail.com>
2019-03-13 23:07:06 -0600

    Add unique key to Card in CardContainer

--- components/CardContainer.js
+++ components/CardContainer.js
@@ -3,13 +3,16 @@ import events from '../static/mock_events.json';
 import Card from '../components/Card';
 
 export default function cardContainer() {
-  const cards = events.map(event => <Card />);
+  const cards = events.map(event => (
+    <Card cardDetails={event} key={event.id} />
+  ));
   return (
     <div className='card-container'>
       {cards}
       <style>
         {`
         .card-container {
+          margin: 2rem 2rem;
           display: flex;
           flex-direction: row;
           justify-content: space-around;
